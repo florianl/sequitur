b92c58c73993c65ac8660ee59c49186fe8ebe98e
Lehner Florian <dev@der-flo.net>
2014-11-05 20:04:47 +0100

    add dynamic function call
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- exemplum.c
+++ exemplum.c
@@ -1,6 +1,12 @@
 #include <stdlib.h>
+#include <stdio.h>
 
-int main(int argc, char *argv[]){
+int func(int argc, char *argv[]){
+
+	int	i;
+
+	for(i=0; i<argc; i++)
+		fprintf(stderr, "%s\n", argv[i]);
 
 	return EXIT_SUCCESS;
 }
--- sequitur.c
+++ sequitur.c
@@ -1,7 +1,33 @@
 #include <stdlib.h>
 #include <dlfcn.h>
+#include <stdio.h>
 
 int main(int argc, char *argv[]){
+
+	void			*handle;
+	int				(*func)();
+
+
+	handle = dlopen("./exemplum.so", RTLD_NOW);
+
+	if (!handle){
+		fprintf(stderr, "%s\n", dlerror());
+		exit(EXIT_FAILURE);
+	}
+
+	dlerror();
+
+	func = (int (*)()) dlsym(handle,"func");
+	if(!func) {
+		fprintf(stderr, "%s\n", dlerror());
+		exit(EXIT_FAILURE);
+	}
+
+	(*func)(argc, argv);
+
+	printf("functions returns %i\n",(*func)());
+
+	dlerror();
 
 	return EXIT_SUCCESS;
 }
