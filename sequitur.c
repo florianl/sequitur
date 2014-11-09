7a78971a87e143a957c423dcb35a91d372c688c9
Lehner Florian <dev@der-flo.net>
2014-11-09 21:00:43 +0100

    Add parameter -V
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- sequitur.c
+++ sequitur.c
@@ -4,11 +4,20 @@
 #include <pthread.h>
 #include <errno.h>
 #include <string.h>
+#include <unistd.h>
+
+#define	SEQUITUR_VERSION	"0.1"
 
 struct threads{
 	pthread_t		id;
 	int				num;
 };
+
+static void usage(const char *prog){
+	fprintf(stderr, "%s [OPTIONS] module [module [module]\n", prog);
+	fprintf(stderr, "   starts dynamically the given module(s)\n");
+	fprintf(stderr, "   -V\tprint version and exit\n");
+}
 
 int main(int argc, char *argv[]){
 
@@ -18,6 +27,20 @@ int main(int argc, char *argv[]){
 	int				errno;
 	int				i = 0;
 	int				s = 0;
+	int				c = 0;
+
+	while((c = getopt(argc, argv, "V")) != -1){
+		switch(c){
+			case 'V':
+				fprintf(stdout, "sequitur version %s\n", SEQUITUR_VERSION);
+				exit(EXIT_SUCCESS);
+				break;
+			default:
+				usage(argv[0]);
+				exit(EXIT_SUCCESS);
+		}
+	}
+
 
 	thread = calloc(argc+1, sizeof(struct threads));
 	if(!thread){
