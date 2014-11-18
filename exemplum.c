85b4266ef75af9e59a86907744d03650cb26e508
Lehner Florian <dev@der-flo.net>
2014-11-18 20:31:38 +0100

    read from pipeline
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- exemplum.c
+++ exemplum.c
@@ -1,16 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 struct threads{
 	pthread_t		id;
 	int				num;
+	int				pipeline[2];
 };
 
 int func(void *arg){
 
 	struct threads	*thread = arg;
+	char buf;
 
 	fprintf(stdout, "Hello World from %d\n", thread->num);
+
+	while(read(thread->pipeline[0], &buf, 1) > 0)
+		fprintf(stdout, "%c", buf);
+	fprintf(stdout, "\n");
+	close(thread->pipeline[0]);
 
 	return EXIT_SUCCESS;
 }
