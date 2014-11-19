d0289497a2d8fa8ff0c7b63ffa0a93c88b5281c9
Lehner Florian <dev@der-flo.net>
2014-11-19 21:18:37 +0100

    close writting side before reading
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- exemplum.c
+++ exemplum.c
@@ -13,6 +13,8 @@ int func(void *arg){
 	struct threads	*thread = arg;
 	char buf;
 
+	close(thread->pipeline[1]);
+
 	fprintf(stdout, "Hello World from %d\n", thread->num);
 
 	while(read(thread->pipeline[0], &buf, 1) > 0)
